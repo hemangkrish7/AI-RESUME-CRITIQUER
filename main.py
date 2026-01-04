9578322ac42d6586a62505ee1d1d21b35b4a2413
Hemang Krish <99909682+hemangkrish7@users.noreply.github.com>
2026-01-04 10:12:46 +0530

    model updated

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.markdown("Upload your resume in PDF or TXT format and get instant feedback!")
 # ------------------ GEMINI SETUP ------------------
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 # ------------------ FILE UPLOAD ------------------
 uploaded_file = st.file_uploader(
